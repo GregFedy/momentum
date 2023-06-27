97288d9a925756c8aa1f37747191501d1479bfd6
Grigorii <mr.fursy@gmail.com>
2023-06-27 18:52:39 +0300

    add new feature: greeting widget

---
+++ src/js/greeting.js
@@ -0,0 +1,34 @@
+const getTimeOfDay = () => {
+  const hour = new Date().getHours();
+
+  if (hour >= 0 && hour < 6) {
+    return 'night';
+  } else if (hour < 12) {
+    return 'morning';
+  } else if (hour < 18) {
+    return 'afternoon';
+  } else {
+    return 'evening';
+  }
+};
+
+export const showGreeting = (element) => {
+  const date = new Date();
+  const millisecondsUntilNextSecond = 1000 - date.getMilliseconds();
+  const timeOfDay = getTimeOfDay();
+  const greetingText = `Good ${timeOfDay}`;
+  element.textContent = greetingText;
+  setTimeout(() => showGreeting(element), millisecondsUntilNextSecond);
+};
+
+export const setLocalStorage = (name) => {
+  localStorage.setItem('name', name.value);
+};
+
+export const getLocalStorage = (name) => {
+  const nameFromLS = localStorage.getItem('name');
+
+  if (nameFromLS !== null) {
+    name.value = nameFromLS;
+  }
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 import '/src/css/style.css';
 import '/src/css/owfont-regular.css';
 import { showTime, showDate } from './clockCalendar.js';
+import { showGreeting, setLocalStorage, getLocalStorage } from './greeting.js';
 
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
+const greeting = document.querySelector('.greeting');
+const name = document.querySelector('.name');
 
 if (time) {
   showTime(time);
@@ -12,3 +15,14 @@ if (time) {
 if (date) {
   showDate(date);
 }
+
+if (greeting) {
+  showGreeting(greeting);
+}
+
+window.addEventListener('beforeunload', () => {
+  setLocalStorage(name);
+});
+window.addEventListener('load', () => {
+  getLocalStorage(name);
+});
